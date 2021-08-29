eadc4acee43965e44ba74975d9e4a06f466a6263
MaxLmqr <max.lemasquerier@protonmail.com>
2021-08-29 19:01:01 +0200

    <frontend> setup a button to logout

--- frontend/src/screens/main/home.screen.tsx
+++ frontend/src/screens/main/home.screen.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {useDispatch} from 'react-redux';
+import AuthSlice from '../../store/slices/auth.slice';
 import {styles} from './styles';
 
 export const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Button title={'Logout'} onPress={() => dispatch(AuthSlice.actions.reset())} />
     </View>
   );
 };
